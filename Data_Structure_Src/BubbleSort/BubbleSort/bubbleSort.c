6e90b2c09504b3d672e2ca0c4aa3d919100f9bdd
ban <bankiki@naver.com>
2017-11-25 23:57:50 +0900

    버블정렬, 선택정렬, 삽입정렬, 병합정렬

---
+++ Data_Structure_Src/BubbleSort/BubbleSort/bubbleSort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    int temp;
+    
+    for(i=0; i<n-1; i++)
+    {
+        for(j=0; j<(n-i)-1; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+int main(void)
+{
+    int arr[4] = {3, 2, 4, 1};
+    int i;
+    
+    // sizeof(arr) = 16, sizeof(int) = 4
+    // sizeof 자료형의 크기를 얻기위해 쓰임
+    BubbleSort(arr, sizeof(arr)/sizeof(int));
+    
+    for(i=0; i<4; i++)
+        printf("%d ", arr[i]);
+    
+    printf("\n");
+    return 0;
+}
